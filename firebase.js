846573e32df995bd8ed1673b3d4e27eab4512db5
Sonam Yadav <yadusonam08@gmail.com>
2026-01-05 23:42:50 +0530

    feat: Integrate Firebase for backend services
    Adds Firebase initialization to the application. This prepares the project for future integration with Firebase services like authentication and database.

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEPHzlhJixoK0M4w70d3josThrvtTCJyU",
+  authDomain: "quadx-6ad7e.firebaseapp.com",
+  projectId: "quadx-6ad7e",
+  storageBucket: "quadx-6ad7e.firebasestorage.app",
+  messagingSenderId: "425711577256",
+  appId: "1:425711577256:web:7d8fd0c6d49758eefe3a9d"
+};
+
+export const app = initializeApp(firebaseConfig);
